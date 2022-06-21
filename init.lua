7750f3a9d32c55c6c23c9bc15d5c3ee6307732c4
Ahmed Rizwan <ahmedrizwan@outlook.com>
2022-06-21 07:13:33 +0500

    feat: setup debugger for node-bifrost (typescript)

--- init.lua
+++ init.lua
@@ -19,6 +19,7 @@ dap.adapters.node2 = {
   command = "node",
   args = {os.getenv("HOME") .. "/vscode-node-debug2/out/src/nodeDebug.js"},
 }
+
 dap.configurations.javascript = {
   {
     name = "Launch",
@@ -37,6 +38,59 @@ dap.configurations.javascript = {
     request = "attach",
     processId = require"dap.utils".pick_process,
   },
+}
+
+dap.configurations.typescript = { -- works for node-bifrost project
+  {
+    name = "Launch",
+    type = "node2",
+    request = "launch",
+    program = "${workspaceFolder}/packages/bifrost-server/dist/index.js",
+    preLaunchTask = "yarn dev",
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = "inspector",
+    console = "integratedTerminal",
+  },
+  {
+    -- For this to work you need to make sure the node process is started with the `--inspect` flag.
+    name = "Attach to process",
+    type = "node2",
+    request = "attach",
+    processId = require"dap.utils".pick_process,
+  },
+}
+
+dap.adapters.chrome = {
+    type = "executable",
+    command = "node",
+    args = {os.getenv("HOME") .. "/vscode-chrome-debug/out/src/chromeDebug.js"}
+}
+
+dap.configurations.javascriptreact = { -- change this to javascript if needed
+    {
+        type = "chrome",
+        request = "attach",
+        program = "${file}",
+        cwd = vim.fn.getcwd(),
+        sourceMaps = true,
+        protocol = "inspector",
+        port = 9222,
+        webRoot = "${workspaceFolder}",
+    }
+}
+
+dap.configurations.typescriptreact = { -- change to typescript if needed
+    {
+        type = "chrome",
+        request = "attach",
+        program = "${file}",
+        cwd = vim.fn.getcwd(),
+        sourceMaps = true,
+        protocol = "inspector",
+        port = 9222,
+        webRoot = "${workspaceFolder}",
+    }
 }
 
 local dapui = require("dapui")
--- mappings.lua
+++ mappings.lua
@@ -59,30 +59,61 @@ M.nvterm = {
 
 M.nvim_dap = {
    n = {
-      ["<leader>db"] = {
+      ["<leader>dd"] = {
          function()
             require("dap").toggle_breakpoint()
          end,
          "   toggle breakpoint",
       },
-      ["<leader>do"] = {
+      ["<leader>dr"] = {
          function()
             require("dap").repl.open()
          end,
          "   open repl",
       },
-      ["<leader>dc"] = {
+      ["<leader>di"] = {
+         function()
+            require("dap.ui.widgets").hover()
+         end,
+         "   hover",
+      },
+      ["<leader>dj"] = {
+         function()
+            require("dap").step_over()
+         end,
+         "   step over",
+      },
+      ["<leader>dk"] = {
+         function()
+            require("dap").step_out()
+         end,
+         "   step out",
+      },
+      ["<leader>dl"] = {
+         function()
+            require("dap").step_into()
+         end,
+         "   step into",
+      },
+      ["<leader>dh"] = {
          function()
             require("dap").continue()
          end,
          "   continue",
       },
-      ["<leader>dl"] = {
+      ["<leader>dx"] = {
          function()
-            require("dap").run_last()
+            require("dap").terminate()
          end,
-         "   run last",
+         "   terminate",
       },
+   }
+}
+
+M.buffers = {
+   n = {
+      ["<leader>bn"] = { "<cmd> bn <CR>", "  next buffer" },
+      ["<leader>bp"] = { "<cmd> bn <CR>", "  previous buffer" },
    }
 }
 
